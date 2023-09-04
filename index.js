b9f4ed900f99df04b7b2a40184a30c3b37bf3eb5
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-09-04 18:23:46 +0500

    Configur Linters

--- index.js
+++ index.js
@@ -1,44 +1,210 @@
-const fromcurrency = document.getElementById("from-currency");
-const tocurrency = document.getElementById("to-currency");
-const api = ` https://v6.exchangerate-api.com/v6/08f80a967dd8e8fb7b87aba1/latest/USD`;
+const currencies = [
+  'USD',
+  'AED',
+  'AFN',
+  'ALL',
+  'AMD',
+  'ANG',
+  'AOA',
+  'ARS',
+  'AUD',
+  'AWG',
+  'AZN',
+  'BAM',
+  'BBD',
+  'BDT',
+  'BGN',
+  'BHD',
+  'BIF',
+  'BMD',
+  'BND',
+  'BOB',
+  'BRL',
+  'BSD',
+  'BTN',
+  'BWP',
+  'BYN',
+  'BZD',
+  'CAD',
+  'CDF',
+  'CHF',
+  'CLP',
+  'CNY',
+  'COP',
+  'CRC',
+  'CUP',
+  'CVE',
+  'CZK',
+  'DJF',
+  'DKK',
+  'DOP',
+  'DZD',
+  'EGP',
+  'ERN',
+  'ETB',
+  'EUR',
+  'FJD',
+  'FKP',
+  'FOK',
+  'GBP',
+  'GEL',
+  'GGP',
+  'GHS',
+  'GIP',
+  'GMD',
+  'GNF',
+  'GTQ',
+  'GYD',
+  'HKD',
+  'HNL',
+  'HRK',
+  'HTG',
+  'HUF',
+  'IDR',
+  'ILS',
+  'IMP',
+  'INR',
+  'IQD',
+  'IRR',
+  'ISK',
+  'JEP',
+  'JMD',
+  'JOD',
+  'JPY',
+  'KES',
+  'KGS',
+  'KHR',
+  'KID',
+  'KMF',
+  'KRW',
+  'KWD',
+  'KYD',
+  'KZT',
+  'LAK',
+  'LBP',
+  'LKR',
+  'LRD',
+  'LSL',
+  'LYD',
+  'MAD',
+  'MDL',
+  'MGA',
+  'MKD',
+  'MMK',
+  'MNT',
+  'MOP',
+  'MRU',
+  'MUR',
+  'MVR',
+  'MWK',
+  'MXN',
+  'MYR',
+  'MZN',
+  'NAD',
+  'NGN',
+  'NIO',
+  'NOK',
+  'NPR',
+  'NZD',
+  'OMR',
+  'PAB',
+  'PEN',
+  'PGK',
+  'PHP',
+  'PKR',
+  'PLN',
+  'PYG',
+  'QAR',
+  'RON',
+  'RSD',
+  'RUB',
+  'RWF',
+  'SAR',
+  'SBD',
+  'SCR',
+  'SDG',
+  'SEK',
+  'SGD',
+  'SHP',
+  'SLE',
+  'SLL',
+  'SOS',
+  'SRD',
+  'SSP',
+  'STN',
+  'SYP',
+  'SZL',
+  'THB',
+  'TJS',
+  'TMT',
+  'TND',
+  'TOP',
+  'TRY',
+  'TTD',
+  'TVD',
+  'TWD',
+  'TZS',
+  'UAH',
+  'UGX',
+  'UYU',
+  'UZS',
+  'VES',
+  'VND',
+  'VUV',
+  'WST',
+  'XAF',
+  'XCD',
+  'XDR',
+  'XOF',
+  'XPF',
+  'YER',
+  'ZAR',
+  'ZMW',
+  'ZWL',
+];
+
+const fromcurrency = document.getElementById('from-currency');
+const tocurrency = document.getElementById('to-currency');
+const api = ' https://v6.exchangerate-api.com/v6/08f80a967dd8e8fb7b87aba1/latest/USD';
+const result = document.querySelector('#result');
 
 currencies.forEach((element) => {
-  const option = document.createElement("option");
+  const option = document.createElement('option');
   option.value = element;
   option.text = element;
   fromcurrency.appendChild(option);
 });
 
 currencies.forEach((element) => {
-  const option = document.createElement("option");
+  const option = document.createElement('option');
   option.value = element;
   option.text = element;
   tocurrency.appendChild(option);
 });
 
-fromcurrency.value = "USD";
-tocurrency.value = "AFN";
+fromcurrency.value = 'USD';
+tocurrency.value = 'AFN';
 
-let convertCurrency = () => {
-  const amount = document.querySelector("#amount-input").value;
-  const from_currency = fromcurrency.value;
-  const to_currency = tocurrency.value;
+const convertCurrency = () => {
+  const amount = document.querySelector('#amount-input').value;
+  const fromCurrency = fromcurrency.value;
+  const toCurrency = tocurrency.value;
 
-  if (amount.length != 0) {
+  if (amount.length !== 0) {
     fetch(api)
       .then((resp) => resp.json())
       .then((data) => {
-        let fromExchange = data.conversion_rates[from_currency];
-        let toExchange = data.conversion_rates[to_currency];
+        const fromExchange = data.conversion_rates[fromCurrency];
+        const toExchange = data.conversion_rates[toCurrency];
 
         const convertedAmount = (amount / fromExchange) * toExchange;
 
-        result.innerHTML = `${amount} ${from_currency} = ${convertedAmount.toFixed(2)} ${to_currency}`
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(2)} ${toCurrency}`;
       });
   } else {
-    alert("Please fill in the amount");
+    alert('Please fill in the amount');
   }
 };
 
-document.querySelector("#btn").addEventListener("click", convertCurrency);
-window.addEventListener("load", convertCurrency);
+document.querySelector('#btn').addEventListener('click', convertCurrency);
+window.addEventListener('load', convertCurrency);
